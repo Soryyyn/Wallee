ff21ec0349f1421109f5dd6744f6ee7274a38808
Soryyyn <soryn.baechli@gmail.com>
2021-02-26 16:33:47 +0100

    ✨ added startup stuff

--- src/settings.ts
+++ src/settings.ts
@@ -2,9 +2,11 @@ import fs from "fs";
 import path from "path";
 import chalk from "chalk";
 import inquirer from "inquirer";
+import os from "os";
 
 const configPath: any = path.resolve(process.cwd(), "config.json").toString();
 let config: any; // gets loaded on runtime
+const startupFolderPath = path.join(os.homedir(), "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup");
 
 // load config to object if file exists else create default
 export function loadConfigFile(): any {
@@ -26,11 +28,35 @@ export function loadConfigFile(): any {
       console.log(chalk.redBright(`Couldn't create user configuration file, error: `) + chalk.white(err));
     }
   }
+
+  // check if wallee needs to be added to startup or removed
+  if (getConfig("startup")) addToStartup();
+  else removeFromStartup();
 }
 
 // returns value from specified key in loaded user config
 export function getConfig(setting: any): any {
   return config[setting];
+}
+
+// add batch file to startup folder
+function addToStartup() {
+  if (!fs.existsSync(path.join(startupFolderPath, "wallee-newWallpaper.bat"))) {
+    try {
+      fs.writeFileSync(path.join(startupFolderPath, "wallee-newWallpaper.bat"), "@echo off\nwallee new\nexit");
+
+      console.log(`\n${chalk.greenBright("Successfully")} added Wallee to system startup`);
+    } catch (err: any) {
+      console.log(chalk.redBright(`\nCouldn't add Wallee to startup, error: `) + chalk.white(err));
+    }
+  }
+}
+
+// remove batch file from startup folder
+function removeFromStartup() {
+  if (fs.existsSync(path.join(startupFolderPath, "wallee-newWallpaper.bat"))) {
+    fs.unlinkSync(path.join(startupFolderPath, "wallee-newWallpaper.bat"));
+  }
 }
 
 // change config
